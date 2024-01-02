351b77ccb169d91bff8907e46ca0d723239083a6
Calvin Davis <mynameiscalvindavis@gmail.com>
2024-01-02 16:12:47 -0700

    Add slider size config

--- slider.py
+++ slider.py
@@ -1,7 +1,8 @@
 class Slider:
-    def __init__(self,low,high,default):
+    def __init__(self,low,high,default,linelength=120):
         '''slider has range from low to high
         and is set to default'''
+        self.linelength = linelength
         self.low = low
         self.high = high
         self.val = default
@@ -13,7 +14,7 @@ class Slider:
         self.x = x
         self.y = y
         #the position of the rect you slide:
-        self.rectx = self.x + map(self.val,self.low,self.high,0,120)
+        self.rectx = self.x + map(self.val,self.low,self.high,0,self.linelength)
         self.recty = self.y - 10
         
     def value(self):
@@ -21,22 +22,22 @@ class Slider:
         #gray line behind slider
         strokeWeight(4)
         stroke(200)
-        line(self.x,self.y,self.x + 120,self.y)
+        line(self.x,self.y,self.x + self.linelength,self.y)
         #press mouse to move slider
         if mousePressed and dist(mouseX,mouseY,self.rectx,self.recty) < 20:
             self.rectx = mouseX
         #constrain rectangle
-        self.rectx = constrain(self.rectx, self.x, self.x + 120)
+        self.rectx = constrain(self.rectx, self.x, self.x + self.linelength)
         #draw rectangle
         strokeWeight(1)
         stroke(0)
         fill(255)
         rect(self.rectx,self.recty,10,20)
-        self.val = map(self.rectx,self.x,self.x + 120,self.low,self.high)
+        self.val = map(self.rectx,self.x,self.x + self.linelength,self.low,self.high)
         #draw label
         fill(0)
         textSize(10)
         text(int(self.val),self.rectx,self.recty + 35)
         #text label
-        text(self.label,self.x + 135,self.y);
+        text(self.label,self.x + self.linelength + 15,self.y);
         return self.val
